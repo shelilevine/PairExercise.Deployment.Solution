4ce71f20706bd58d2abb45c6f1676f97258754ce
shelilevine <shelisteinberg@gmail.com>
2020-06-05 15:05:50 -0400

    update tests

--- server/index.test.js
+++ server/index.test.js
@@ -8,15 +8,30 @@ const seedUsers = require('../script/users.json')
 describe('GET /users', () => {
 
   before(async (done) => {
-    await db.sync({ force: true })
-    await db.models.user.bulkCreate(seedUsers)
-    done();
+    try {
+      await db.sync({ force: true })
+      await db.models.user.bulkCreate(seedUsers)
+      done()
+    } catch (err) {
+      done(err);
+    }
+    // await db.sync({ force: true })
+    // await db.models.user.bulkCreate(seedUsers)
   })
 
   it('should return list of users', async (done) => {
-    const res = await request(app).get('/api/users')
-    expect(res.status).to.equal(200)
-    expect(res.body.length).to.equal(seedUsers.length)
-    done();
+    try {
+      const res = await request(app).get('/api/users')
+      expect(res.status).to.equal(200)
+      expect(res.body.length).to.equal(seedUsers.length)
+      done()
+    } catch(err) {
+      done(err)
+    }
+
+
+    // const res = await request(app).get('/api/users')
+    // expect(res.status).to.equal(200)
+    // expect(res.body.length).to.equal(seedUsers.length)
   })
 })
